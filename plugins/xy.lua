3bd174fc1a32334c4f8b8c8fdd3e2d864c0e2cb2
sina021 <sina021370@gmail.com>
2016-04-18 13:12:51 +0430

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,8 +1,8 @@
 do
 local function run(msg, matches)
-local bot_id = 198434641
+local bot_id = 205692043
 local x = 180836680
-local y = 205692043
+local y = 198434641
 local z = 201318555
 local w = 198964014
 
